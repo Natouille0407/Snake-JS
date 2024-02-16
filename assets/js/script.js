28bcadc2432df0d9f8509d16a90b75ddc2158f5f
Natouille <ngouveia@edenschool.fr>
2024-02-16 12:32:28 +0100

    Added Image

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,25 @@ let gamearea = document.querySelector(".gameArea");
 function moveUp(snakeHead) {
     let currentPosition = parseFloat(getComputedStyle(snakeHead).top);
     snakeHead.style.top = currentPosition - 20 + "px";
+    snakeHead.style.rotate = 0 + "deg";
 }
 
 function moveDown(snakeHead) {
     let currentPosition = parseFloat(getComputedStyle(snakeHead).top);
     snakeHead.style.top = currentPosition + 20 + "px";
+    snakeHead.style.rotate = 180 + "deg";
 }
 
 function moveLeft(snakeHead) {
     let currentPosition = parseFloat(getComputedStyle(snakeHead).left);
     snakeHead.style.left = currentPosition - 20 + "px";
+    snakeHead.style.rotate = -90 + "deg";
 }
 
 function moveRight(snakeHead) {
     let currentPosition = parseFloat(getComputedStyle(snakeHead).left);
     snakeHead.style.left = currentPosition + 20 + "px";
+    snakeHead.style.rotate = 90 + "deg";
 }
 
 function moveSnake() {
@@ -41,6 +45,8 @@ function moveSnake() {
             break;
     }
 
+    checkBoundaries();
+
     for (let i = snakeSegments.length - 1; i > 0; i--) {
         let currentSegment = snakeSegments[i];
         let previousSegment = snakeSegments[i - 1];
@@ -54,6 +60,32 @@ function moveSnake() {
         firstSegment.style.left = snakeHead.offsetLeft + "px";
     }
 }
+
+
+function checkBoundaries() {
+    let snakeHead = document.querySelector(".snake-head");
+    let gameArea = document.querySelector(".gameArea");
+    
+    let headRect = snakeHead.getBoundingClientRect();
+    let areaRect = gameArea.getBoundingClientRect();
+
+    if (headRect.left < areaRect.left - 3) {
+        snakeHead.style.left = areaRect.left - 300 + "px";
+    }
+
+    if (headRect.right > areaRect.right) {
+        snakeHead.style.left = (areaRect.right - snakeHead.offsetWidth - 320) + "px";
+    }
+
+    if (headRect.top < areaRect.top ) {
+        snakeHead.style.top = areaRect.top + "px";
+    }
+
+    if (headRect.bottom > areaRect.bottom) {
+        snakeHead.style.top = (areaRect.bottom - snakeHead.offsetHeight - 20) + "px";
+    }
+}
+
 
 function changeDirection(newDirection) {
     snakeDirection = newDirection;
@@ -77,21 +109,25 @@ function handleDirectionChange(event) {
 }
 
 function appleRandomSpawn() {
-    let applePosY = Math.floor(Math.random() * (715 - (-35) + 1)) + (-35);
-    let applePosX = Math.floor(Math.random() * (750 - 5 + 1)) + 5;
+    let applePosY = Math.floor(Math.random() * (760 + 1));
+    let applePosX = Math.floor(Math.random() * (760 + 1));
     document.getElementById('pomme').style.top = applePosY + "px";
     document.getElementById('pomme').style.left = applePosX + "px";
+    
+    if(checkAppleWallCollision()) {
+        appleRandomSpawn()
+    }
 }
 
 function wallRandomSpawn() {
-    let wallPosY = Math.floor(Math.random() * (715 - (-35) + 1)) + (-35);
-    let wallPosX = Math.floor(Math.random() * (750 - 5 + 1)) + 5;
+    let wallPosY = Math.floor(Math.random() * (760 + 1));
+    let wallPosX = Math.floor(Math.random() * (760 + 1));
     let wall = document.createElement('div');
     gamearea.appendChild(wall);
     wall.classList.add('wall');
 
-    wall.style.top = wallPosY + "px"
-    wall.style.right = wallPosX + "px"
+    wall.style.top = wallPosY + "px";
+    wall.style.left = wallPosX + "px";
 }
 
 function wallremove() {
@@ -99,6 +135,72 @@ function wallremove() {
     for (let i = 0; i < walls.length; i++) {
         walls[i].remove();
     }
+}
+
+function checkAppleWallCollision() {
+    let pomme = document.getElementById('pomme');
+    let walls = document.querySelectorAll('.wall');
+
+    let pommeRect = pomme.getBoundingClientRect();
+
+    for (let i = 0; i < walls.length; i++) {
+        let wallRect = walls[i].getBoundingClientRect();
+
+        if (
+            pommeRect.top < wallRect.bottom &&
+            pommeRect.bottom > wallRect.top &&
+            pommeRect.left < wallRect.right &&
+            pommeRect.right > wallRect.left
+        ) {
+            
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function avoidWallOverlap() {
+    let collisionDetected = true;
+    let wallPosX, wallPosY;
+
+    while (collisionDetected) {
+
+        wallPosX = Math.floor(Math.random() * (760 + 1));
+        wallPosY = Math.floor(Math.random() * (365 * 2 + 1)) - 365;
+
+        collisionDetected = checkCollisionWithWalls(wallPosX, wallPosY);
+    }
+
+    createWallAtPosition(wallPosX, wallPosY);
+}
+
+function checkCollisionWithWalls(posX, posY) {
+    let pommeRect = document.getElementById('pomme').getBoundingClientRect();
+    let walls = document.querySelectorAll('.wall');
+    
+    for (let i = 0; i < walls.length; i++) {
+        let wallRect = walls[i].getBoundingClientRect();
+        
+        if (
+            posY < wallRect.bottom &&
+            posY + 30 > wallRect.top &&
+            posX < wallRect.right &&
+            posX + 30 > wallRect.left
+        ) {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+function createWallAtPosition(posX, posY) {
+    let wall = document.createElement('div');
+    wall.classList.add('wall');
+    wall.style.left = posX + 'px';
+    wall.style.top = posY + 'px';
+    gamearea.appendChild(wall);
 }
 
 function scoreIncrement() {
